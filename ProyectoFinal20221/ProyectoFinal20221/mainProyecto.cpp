39f0a3b9f95818d315fb2d73cb08c9669cd4fc72
Daniel Torres <rebel.danieltorres@gmail.com>
2021-10-03 20:38:28 -0500

    The first commit

---
+++ ProyectoFinal20221/ProyectoFinal20221/mainProyecto.cpp
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------*/
+/* ----------------   Proyecto Final ----------------------*/
+/*---Computación Gráfica e Interacción Humano-Computadora--*/
+/*-----------------    2022-1   ---------------------------*/
+/*----------- Alumnos: Yolanda------------*/
+/*----------Herrera Godina Adriana Jocelyn-----------------*/
+/*---------------Sanchez Torres Sergio Daniel--------------*/
